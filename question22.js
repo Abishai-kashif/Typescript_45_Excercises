22861d6bd15538484d31b93c40299e900525bbdf
Abishai-kashif <abishaikashif@gmail.com>
2024-04-04 19:53:23 +0500

    Assignment number 22

---
+++ question22.js
@@ -0,0 +1,4 @@
+//Intentional Error
+var languages = ["English", "Urdu", "Punjabi", "Sindhi", "Pashto"];
+console.log("My favorite Languages is ".concat(languages[100])); //Index Error: 100 is not a valid index
+languages[3] = "Sindhi"; //correcting the error by excessing the correct index value
