20a052663cd41466c3fc358d2a3a2a060f8c2a6f
Abishai-kashif <abishaikashif@gmail.com>
2024-04-04 19:48:00 +0500

    Assignment number 22

---
+++ quesiton22.ts
@@ -0,0 +1,8 @@
+//Intentional Error
+
+let languages: string[] = ["English" ,"Urdu" ,"Punjabi" ,"Sindhi" ,"Pashto"];
+ 
+console.log(`My favorite Languages is ${languages[100]}`); //Index Error: 100 is not a valid index
+
+
+languages[3] = "Sindhi"; //correcting the error by excessing the correct index value
